302237d97529a8363a401f2a878b4147498b375e
Daniel Limberger <cgdaniel@outlook.com>
2018-03-19 10:37:10 +0100

    migrate to webgl-operate 0.2.0

--- source/cornell-box/cornellrenderer.ts
+++ source/cornell-box/cornellrenderer.ts
@@ -1,5 +1,8 @@
 
-import * as gloperate from 'webgl-operate';
+import {
+    AccumulatePass, auxiliaries, BlitPass, Camera, Context, DefaultFramebuffer, Framebuffer, Invalidate,
+    MouseEventProvider, NdcFillingTriangle, Program, Renderbuffer, Renderer, Shader, Texture2,
+} from 'webgl-operate';
 
 import { vec3, vec4 } from 'gl-matrix';
 
@@ -21,16 +24,16 @@ const _gUp = vec3.fromValues(
 const _gFovy = 37.0;
 
 
-export class CornellRenderer extends gloperate.AbstractRenderer {
+export class CornellRenderer extends Renderer {
 
     protected _extensions = false;
 
     // stuff
-    protected _camera: gloperate.Camera;
-    protected _ndcTriangle: gloperate.NdcFillingTriangle;
+    protected _camera: Camera;
+    protected _ndcTriangle: NdcFillingTriangle;
 
     // program and uniforms
-    protected _program: gloperate.Program;
+    protected _program: Program;
     protected _uTransform: WebGLUniformLocation;
 
     protected _uFrame: WebGLUniformLocation;
@@ -39,34 +42,35 @@ export class CornellRenderer extends gloperate.AbstractRenderer {
     protected _uViewport: WebGLUniformLocation;
 
     // Textures
-    protected _verticesImage: gloperate.Texture2;
-    protected _indicesImage: gloperate.Texture2;
-    protected _colorsImage: gloperate.Texture2;
+    protected _verticesImage: Texture2;
+    protected _indicesImage: Texture2;
+    protected _colorsImage: Texture2;
 
-    protected _hsphereImage: gloperate.Texture2;
-    protected _lightsImage: gloperate.Texture2;
+    protected _hsphereImage: Texture2;
+    protected _lightsImage: Texture2;
 
     // blit and accumulate
-    protected _accumulate: gloperate.AccumulatePass;
-    protected _blit: gloperate.BlitPass;
+    protected _accumulate: AccumulatePass;
+    protected _blit: BlitPass;
 
-    protected _defaultFBO: gloperate.DefaultFramebuffer;
-    protected _colorRenderTexture: gloperate.Texture2;
-    protected _depthRenderbuffer: gloperate.Renderbuffer;
-    protected _intermediateFBO: gloperate.Framebuffer;
+    protected _defaultFBO: DefaultFramebuffer;
+    protected _colorRenderTexture: Texture2;
+    protected _depthRenderbuffer: Renderbuffer;
+    protected _intermediateFBO: Framebuffer;
 
 
-    protected onUpdate(): void {
-        if (!this._altered.any) {
-            return;
-        }
+    protected onUpdate(): boolean {
+        return this._altered.any;
+    }
 
-        const gl = this.context.gl;
-        const gl2facade = this.context.gl2facade;
+    protected onPrepare(): void {
+
+        const gl = this._context.gl;
+        const gl2facade = this._context.gl2facade;
 
         if (!this._intermediateFBO.initialized) {
             this._colorRenderTexture.initialize(this._frameSize[0], this._frameSize[1],
-                this.context.isWebGL2 ? gl.RGBA8 : gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE);
+                this._context.isWebGL2 ? gl.RGBA8 : gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE);
             this._depthRenderbuffer.initialize(this._frameSize[0], this._frameSize[1], gl.DEPTH_COMPONENT16);
             this._intermediateFBO.initialize([[gl2facade.COLOR_ATTACHMENT0, this._colorRenderTexture]
                 , [gl.DEPTH_ATTACHMENT, this._depthRenderbuffer]]);
@@ -85,7 +89,7 @@ export class CornellRenderer extends gloperate.AbstractRenderer {
     }
 
     protected onFrame(frameNumber: number): void {
-        const gl = this.context.gl;
+        const gl = this._context.gl;
 
         gl.viewport(0, 0, this._frameSize[0], this._frameSize[1]);
         this._camera.viewport = [this._frameSize[0], this._frameSize[1]];
@@ -130,24 +134,21 @@ export class CornellRenderer extends gloperate.AbstractRenderer {
     }
 
 
-    initialize(context: gloperate.Context, callback: gloperate.Invalidate): boolean {
-        if (!super.initialize(context, callback)) {
-            return false;
-        }
-        const gl = this.context.gl;
-        const gl2facade = this.context.gl2facade;
+    protected onInitialize(context: Context, callback: Invalidate, mouseEventProvider: MouseEventProvider): boolean {
+        const gl = this._context.gl;
+        const gl2facade = this._context.gl2facade;
 
         /* Enable required extensions. */
 
-        if (this._extensions === false && this.context.isWebGL1) {
-            gloperate.auxiliaries.assert(this.context.supportsStandardDerivatives,
+        if (this._extensions === false && this._context.isWebGL1) {
+            auxiliaries.assert(this._context.supportsStandardDerivatives,
                 `expected OES_standard_derivatives support`);
             /* tslint:disable-next-line:no-unused-expression */
-            this.context.standardDerivatives;
+            this._context.standardDerivatives;
             this._extensions = true;
         }
 
-        this._camera = new gloperate.Camera();
+        this._camera = new Camera();
         this._camera.eye = _gEye;
         this._camera.center = _gCenter;
         this._camera.up = _gUp;
@@ -156,11 +157,11 @@ export class CornellRenderer extends gloperate.AbstractRenderer {
         this._camera.fovy = _gFovy;
 
         // program
-        const vert = new gloperate.Shader(this.context, gl.VERTEX_SHADER, 'cornell.vert');
+        const vert = new Shader(this._context, gl.VERTEX_SHADER, 'cornell.vert');
         vert.initialize(require('./cornell.vert'));
-        const frag = new gloperate.Shader(this.context, gl.FRAGMENT_SHADER, 'cornell.frag');
+        const frag = new Shader(this._context, gl.FRAGMENT_SHADER, 'cornell.frag');
         frag.initialize(require('./cornell.frag'));
-        this._program = new gloperate.Program(this.context);
+        this._program = new Program(this._context);
         this._program.initialize([vert, frag]);
 
         // uniforms
@@ -179,24 +180,24 @@ export class CornellRenderer extends gloperate.AbstractRenderer {
         this._program.unbind();
 
         // triangle
-        this._ndcTriangle = new gloperate.NdcFillingTriangle(this.context);
+        this._ndcTriangle = new NdcFillingTriangle(this._context);
         const aVertex = this._program.attribute('a_vertex', 0);
         this._ndcTriangle.initialize(aVertex);
 
         // scene textures
-        this._verticesImage = new gloperate.Texture2(this.context, 'verticesImage');
+        this._verticesImage = new Texture2(this._context, 'verticesImage');
         this._verticesImage.initialize(vertices.length / 3, 1, gl.RGB32F, gl.RGB, gl.FLOAT); // height 1
         this._verticesImage.data(vertices);
         this._verticesImage.wrap(gl.CLAMP_TO_EDGE, gl.CLAMP_TO_EDGE);
         this._verticesImage.filter(gl.NEAREST, gl.NEAREST);
 
-        this._indicesImage = new gloperate.Texture2(this.context, 'indicesImage');
+        this._indicesImage = new Texture2(this._context, 'indicesImage');
         this._indicesImage.initialize(indices.length / 4, 1, gl.RGBA8UI, gl.RGBA_INTEGER, gl.UNSIGNED_BYTE); // height 1
         this._indicesImage.data(indices);
         this._indicesImage.wrap(gl.CLAMP_TO_EDGE, gl.CLAMP_TO_EDGE);
         this._indicesImage.filter(gl.NEAREST, gl.NEAREST);
 
-        this._colorsImage = new gloperate.Texture2(this.context, 'colorsImage');
+        this._colorsImage = new Texture2(this._context, 'colorsImage');
         this._colorsImage.initialize(colors.length / 4
             , 1, gl.RGBA32F, gl.RGBA, gl.FLOAT); // height 1
         this._colorsImage.data(colors);
@@ -212,7 +213,7 @@ export class CornellRenderer extends gloperate.AbstractRenderer {
             spherePoints[3 * i + 1] = points[i][1];
             spherePoints[3 * i + 2] = points[i][2];
         }
-        this._hsphereImage = new gloperate.Texture2(this.context, 'hsphereImage');
+        this._hsphereImage = new Texture2(this._context, 'hsphereImage');
         this._hsphereImage.initialize(samplerSize, samplerSize, gl.RGB32F, gl.RGB, gl.FLOAT);
         this._hsphereImage.data(spherePoints);
         this._hsphereImage.wrap(gl.CLAMP_TO_EDGE, gl.CLAMP_TO_EDGE);
@@ -229,27 +230,27 @@ export class CornellRenderer extends gloperate.AbstractRenderer {
             lights2[i2++] = light[1];
             lights2[i2++] = light[2];
         }
-        this._lightsImage = new gloperate.Texture2(this.context, 'lightsImage');
+        this._lightsImage = new Texture2(this._context, 'lightsImage');
         this._lightsImage.initialize(32, 32, gl.RGB32F, gl.RGB, gl.FLOAT);
         this._lightsImage.data(lights2);
         this._lightsImage.wrap(gl.CLAMP_TO_EDGE, gl.CLAMP_TO_EDGE);
         this._lightsImage.filter(gl.NEAREST, gl.NEAREST);
 
         // framebuffers, textures, and render buffers
-        this._defaultFBO = new gloperate.DefaultFramebuffer(this.context, 'DefaultFBO');
+        this._defaultFBO = new DefaultFramebuffer(this._context, 'DefaultFBO');
         this._defaultFBO.initialize();
-        this._colorRenderTexture = new gloperate.Texture2(this.context, 'ColorRenderTexture');
-        this._depthRenderbuffer = new gloperate.Renderbuffer(this.context, 'DepthRenderbuffer');
-        this._intermediateFBO = new gloperate.Framebuffer(this.context, 'IntermediateFBO');
+        this._colorRenderTexture = new Texture2(this._context, 'ColorRenderTexture');
+        this._depthRenderbuffer = new Renderbuffer(this._context, 'DepthRenderbuffer');
+        this._intermediateFBO = new Framebuffer(this._context, 'IntermediateFBO');
 
         // accumulation
-        this._accumulate = new gloperate.AccumulatePass(this.context);
+        this._accumulate = new AccumulatePass(this._context);
         this._accumulate.initialize(this._ndcTriangle);
         this._accumulate.precision = this._framePrecision;
         this._accumulate.texture = this._colorRenderTexture;
 
         // blit
-        this._blit = new gloperate.BlitPass(this.context);
+        this._blit = new BlitPass(this._context);
         this._blit.initialize(this._ndcTriangle);
         this._blit.readBuffer = gl2facade.COLOR_ATTACHMENT0;
         this._blit.drawBuffer = gl.BACK;
@@ -258,9 +259,7 @@ export class CornellRenderer extends gloperate.AbstractRenderer {
         return true;
     }
 
-    uninitialize(): void {
-        super.uninitialize();
-
+    protected onUninitialize(): void {
         this._program.uninitialize();
         this._ndcTriangle.uninitialize();
 
