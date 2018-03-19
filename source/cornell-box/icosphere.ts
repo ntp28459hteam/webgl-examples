fc337a423ee539a2d0ae0c5ee724e0156fdaaabb
Chris <christopher.weyand@student.hpi.de>
2018-03-19 09:05:45 +0100

    random sampler

--- source/cornell-box/icosphere.ts
+++ source/cornell-box/icosphere.ts
@@ -9,12 +9,19 @@ export function pointsOnSphere(minN: number): Array<vec3> {
 
     // random directions in tangent space
     const donkey = new Array<vec3>();
+
     for (let i = 0; i < 1024; ++i) {
-        donkey.push(vec3.normalize(vec3.create(), vec3.fromValues(
-            auxiliaries.rand(-0.6, 0.6),
-            auxiliaries.rand(0.1, 0.6), // only in positive normal direction
-            auxiliaries.rand(-0.6, 0.6),
-        )));
+        const bound = 0.95;
+        const x = auxiliaries.rand(-bound, bound);
+        const z = auxiliaries.rand(-bound, bound);
+        const y = Math.sqrt(Math.max(1.0 - x * x - z * z, 0.1));
+        donkey.push(vec3.normalize(vec3.create(), vec3.fromValues(x, y, z)));
+
+        // donkey.push(vec3.normalize(vec3.create(), vec3.fromValues(
+        //     auxiliaries.rand(-0.6, 0.6),
+        //     auxiliaries.rand(0.1, 0.6), // only in positive normal direction
+        //     auxiliaries.rand(-0.6, 0.6),
+        // )));
     }
 
     return donkey;
